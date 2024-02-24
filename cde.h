fea8790e07cfaed008ac03baf5e8d5b92825a8e0
ShySpaceSheep <theminecraftf303@gmail.com>
2024-02-24 20:00:26 +0800

    Added core CODE interpreter files
    + Added cde.c and cde.h as core interpreter files
    * Modified the variable.cde test

---
+++ cde.h
@@ -0,0 +1,23 @@
+#ifndef cde_h
+#define cde_h
+
+/* Version information */
+#define CDE_VERSION_MAJOR 0
+#define CDE_VERSION_MINOR 1
+#define CDE_VERSION_PATCH 0
+
+/* Thread status */
+#define CDE_OK 1
+#define CDE_ERRUN 2
+#define CDE_ERRSYNTAX 3
+
+/* Basic types */
+#define CDE_TNONE (-1)
+#define CDE_TBOOLEAN (0)
+#define CDE_TINTEGER (1)
+#define CDE_TCHARA (2)
+#define CDE_TSTRING (3)
+
+#define MAX_BUFFER_SIZE 512
+
+#endif
