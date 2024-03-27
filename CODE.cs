e86a9a55c661ef829f9784991e12dd43e12f99f6
ShySpaceSheep <theminecraftf303@gmail.com>
2024-03-27 23:59:51 +0800

    Added source tests

---
+++ CODE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CODE_interpreter
+{
+    public class CODE
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
