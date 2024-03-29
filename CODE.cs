1352918feef68c64d91e9c6d053bc3e5b62eca6f
ShySpaceSheep <theminecraftf303@gmail.com>
2024-03-29 23:59:49 +0800

    Added core interpreter functions

--- CODE.cs
+++ CODE.cs
@@ -1,15 +1,85 @@
-﻿using System;
+﻿using CODE_interpreter.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace CODE_interpreter
 {
     public class CODE
     {
-        static void Main(string[] args)
+        [Flags]
+        public enum ExitType: int
+        {
+            EXIT_SUCCESS = 0,
+            EXIT_ERR_USAGE = 64,
+            EXIT_ERR_SYNTAX = 65,
+            EXIT_ERR_FILE = 72,
+        }
+
+        public static void Main(string[] args)
         {
             if (args.Length > 1)
             {
-                Environment.Exit(1);
+                Console.WriteLine(CODEStrings.InvalidUsage);
+                Environment.Exit((int) ExitType.EXIT_ERR_USAGE);
             }
+            else if (args.Length == 1)
+            {
+                RunSourceFile(args[0]);
+            }
+            else
+            {
+                RunInteractive();
+            }
+        }
+
+        private static void RunSourceFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                byte[] bytes = File.ReadAllBytes(filePath);
+                string content = Encoding.UTF8.GetString(bytes);
+                ExecuteLine(content);
+            } 
+            else
+            {
+                Console.WriteLine(CODEStrings.FileNotFoundL1 + filePath + "\n" + CODEStrings.FileNotFoundL2);
+                Environment.Exit((int)ExitType.EXIT_ERR_FILE);
+            }
+        }
+
+        private static void RunInteractive()
+        {
+            Console.WriteLine(CODEStrings.InterpreterREPL);
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+            for (;;)
+            {
+                Console.Write(">>> ");
+                string line = reader.ReadLine();
+                if (line == null) break;
+                ExecuteLine(line);
+            }
+        }
+
+        private static void ExecuteLine(string line)
+        {
+            List<Token> tokens = Tokenize(line);
+            foreach (Token t in tokens)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+        private static List<Token> Tokenize(string source)
+        {
+            List<Token> tokens = new List<Token>();
+            string[] words = source.Split(' ');
+            foreach (string word in words)
+            {
+                tokens.Add(new Token(word));
+            }
+            return tokens;
         }
     }
 }
---
+++ Parser/Token.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_interpreter.Parser
+{
+    public class Token
+    {
+        enum TokenType
+        {
+            // Reserved structure/control keywords
+            TK_BEGINCDE, TK_ENDCDE,
+            TK_IF, TK_ELIF, TK_ELSE,
+            TK_BEGINIF, TK_ENDIF,
+            TK_WHILE, TK_BEGINWH, TK_ENDWH,
+            TK_COMMENT, TK_EOF,
+
+            // Input/output keywords
+            TK_DISPLAY, TK_SCAN,
+
+            // Keywords for basic operators
+            TK_ADD, TK_SUB, TK_MULT, TK_DIV, TK_MOD,
+            TK_GREATER, TK_LESSER,
+            TK_GEQUAL, TK_LEQUAL,
+            TK_EQUAL, TK_NOEQUAL,
+            TK_AND, TK_OR, TK_NOT,
+
+            // Other available operators (single-character operators)
+            TK_NEWLINE, TK_CONCAT,
+            TK_LPARA, TK_RPARA,
+            TK_LBRACKET, TK_RBRACKET,
+
+            // Keyword for data types and literals
+            TK_IDENTIFIER,
+            TK_INT, TK_CHAR, TK_BOOL, TK_FLOAT,
+        }
+        public string Value { get; set; }
+
+        public Token(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
