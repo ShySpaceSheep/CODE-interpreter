0a97f8bcb7e91a3748bc931ec3849b76498fdca1
ShySpaceSheep <theminecraftf303@gmail.com>
2024-04-03 11:48:29 +0800

    Added AST classes for use in Parser

---
+++ AST/ASTPrinter.cs
@@ -0,0 +1,52 @@
+﻿using CODE_interpreter.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_interpreter.AST
+{
+    public class AstPrinter : Expression.IVisitor<string>
+    {
+        public string Print(Expression expr)
+        {
+            return expr.Accept(this);
+        }
+        public string VisitBinaryExpr(Expression.Binary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGroupingExpr(Expression.Grouping expr)
+        {
+            return Parenthesize("group", expr.Expression);
+        }
+
+        public string VisitLiteralExpr(Expression.Literal expr)
+        {
+            if (expr.Value == null) return "nil";
+            return expr.Value.ToString();
+        }
+
+        public string VisitUnaryExpr(Expression.Unary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Right);
+        }
+
+        private string Parenthesize(string name, params Expression[] exprs)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (Expression expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ AST/Expression.cs
@@ -0,0 +1,85 @@
+﻿using CODE_interpreter.Parser;
+using System;
+using System.Collections.Generic;
+
+namespace CODE_interpreter.AST
+{
+    public abstract class Expression
+    {
+        public interface IVisitor<R>
+        {
+            R VisitBinaryExpr(Binary expr);
+            R VisitGroupingExpr(Grouping expr);
+            R VisitLiteralExpr(Literal expr);
+            R VisitUnaryExpr(Unary expr);
+        }
+
+        public class Binary : Expression
+        {
+            public Binary(Expression left, Token op, Expression right)
+            {
+                this.Left = left;
+                this.Operator = op;
+                this.Right = right;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitBinaryExpr(this);
+            }
+
+            public readonly Expression Left;
+            public readonly Token Operator;
+            public readonly Expression Right;
+        }
+
+        public class Grouping : Expression
+        {
+            public Grouping(Expression expression)
+            {
+                this.Expression = expression;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitGroupingExpr(this);
+            }
+
+            public readonly Expression Expression;
+        }
+
+        public class Literal : Expression
+        {
+            public Literal(object value)
+            {
+                this.Value = value;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitLiteralExpr(this);
+            }
+
+            public readonly object Value;
+        }
+
+        public class Unary : Expression
+        {
+            public Unary(Token op, Expression right)
+            {
+                this.Operator = op;
+                this.Right = right;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitUnaryExpr(this);
+            }
+
+            public readonly Token Operator;
+            public readonly Expression Right;
+        }
+
+        public abstract R Accept<R>(IVisitor<R> visitor);
+    }
+}
