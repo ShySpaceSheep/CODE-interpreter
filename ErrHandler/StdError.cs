3d739d41c9bafe0a1816e3bf08c5d96d7ae3a1f0
ShySpaceSheep <theminecraftf303@gmail.com>
2024-04-21 04:38:33 +0800

    Added a seperate error handler

---
+++ ErrHandler/StdError.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using CODE_interpreter.CODEStrings;
+using CODE_interpreter.Analyzers;
+
+namespace CODE_interpreter
+{
+    public static class StdError
+    {
+        public class ParseError : Exception
+        {
+            public ParseError() : base() { }
+            public ParseError(string message) : base(message) { }
+            public ParseError(string message, Exception inner) : base(message, inner) { }
+        }
+        public class RuntimeError : Exception
+        {
+            public Token Token { get; }
+            public RuntimeError(Token token, string message) : base(message)
+            {
+                Token = token;
+            }
+        }
+
+        private static bool hasRaisedSyntaxError = false;
+        private static bool hasRaisedRuntimeError = false;
+
+        private static void BroadcastError(int line, string where, string message)
+        {
+            Console.WriteLine($"In file \"{CODE.CurrentFile}\" [Line {line}]");
+            Console.Error.WriteLine($"Error {where}: {message}");
+            StdError.HasSyntaxError = true;
+        }
+        public static void ThrowLexerError(int line, string message)
+        {
+            BroadcastError(line, "", message);
+        }
+        public static void ThrowSyntaxError(Token token, string type, string message)
+        {
+
+        }
+        public static ParseError ThrowParseError(Token token, string message)
+        {
+            if (token.TokenType == Token.Type.EOF)
+            {
+                BroadcastError(token.Line, "at end", message);
+            }
+            else
+            {
+                BroadcastError(token.Line, "at '" + token.Lexeme + "'", message);
+            }
+            return new ParseError();
+        }
+        public static void ThrowRuntimeError(StdError.RuntimeError error)
+        {
+            Console.Error.WriteLine($"In file \"{CODE.CurrentFile}\" [Line {error.Token.Line}]\n{error.Message} ");
+            HasRuntimeError = true;
+        }
+        public static void ThrowArgumentError()
+        {
+            Console.Error.WriteLine(UsageError.ArgumentError());
+            System.Environment.Exit((int) CODE.ExitType.EXIT_ERR_USAGE);
+        }
+        public static void ThrowFileNotFound(string filepath)
+        {
+            Console.Error.WriteLine(UsageError.FileNotFoundError(filepath));
+            System.Environment.Exit((int) CODE.ExitType.EXIT_ERR_FILE);
+        }
+        public static bool HasSyntaxError
+        {
+            get { return hasRaisedSyntaxError; }
+            set { hasRaisedSyntaxError = value; }
+        }
+        public static bool HasRuntimeError
+        {
+            get { return hasRaisedRuntimeError; }
+            set { hasRaisedRuntimeError = value; }
+        }
+    }
+}
