fec7d02b59d7d498fb75a794e6b8b0a23f2e0f82
ShySpaceSheep <theminecraftf303@gmail.com>
2024-04-21 04:45:09 +0800

    Updated `Interpreter` to reflect recent additions from `Parser`

--- Interpreter.cs
+++ Interpreter.cs
@@ -1,74 +1,75 @@
-﻿using CODE_interpreter.AST;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using CODE_interpreter.Analyzers;
+using CODE_interpreter.AST;
+
 
 namespace CODE_interpreter
 {
-    public class Interpreter : Expression.IVisitor<Object>
+    public class Interpreter : Expression.IVisitor<Object>, Statement.IVisitor<object>
     {
-        public class RuntimeError : Exception
-        {
-            public Parser.Token Token { get; }
-            public RuntimeError(Parser.Token token, string message) : base(message)
-            {
-                Token = token;
-            }
-        }
+        private readonly Environment Env = new Environment();
 
-        public void Interpret(Expression expression)
+        public void Interpret(List<Statement> statements)
         {
             try
             {
-                Object value = Evaluate(expression);
-                Console.WriteLine(Stringify(value));
+                foreach (Statement statement in statements)
+                {
+                    Execute(statement);
+                }
             }
-            catch (RuntimeError error)
+            catch (StdError.RuntimeError error)
             {
-                CODE.RuntimeError(error);
+                StdError.ThrowRuntimeError(error);
             }
         }
-        public Object VisitLiteralExpr(Expression.Literal expr)
+
+        public Object VisitLiteralExpression(Expression.Literal expr)
         {
             return expr.Value;
         }
 
-        public Object VisitGroupingExpr(Expression.Grouping expr)
+        public Object VisitGroupingExpression(Expression.Grouping expr)
         {
-            return Evaluate(expr.Expression);
+            return Evaluate(expr.Expr);
         }
 
-        public Object VisitUnaryExpr(Expression.Unary expr)
+        public Object VisitUnaryExpression(Expression.Unary expr)
         {
             Object right = Evaluate(expr.Right);
-            switch (expr.Operator.CurrType) 
+            switch (expr.Operator.TokenType) 
             {
-                case Parser.Token.Type.NOT:
+                case Token.Type.NOT:
                     return IsTruthy(right);
-                case Parser.Token.Type.SUB:
+                case Token.Type.SUB:
                     return -(double)right;
             }
             return null;
         }
 
-        private void CheckNumberOperand(Parser.Token op, Object operand)
+        public Object VisitVariableExpression(Expression.Variable expr)
+        {
+            return Env.Get(expr.Name);
+        }
+
+        private void CheckNumberOperand(Token op, Object operand)
         {
             if (operand is Double) return;
-            throw new RuntimeError(op, "Operand must be a number.");
+            throw new StdError.RuntimeError(op, "TypeError: Operand must be a number.");
         }
 
-        private void CheckNumberOperands(Parser.Token op, Object left, Object right)
+        private void CheckNumberOperands(Token op, Object left, Object right)
         {
-            if (left is Double && right is Double) return;
-            throw new RuntimeError(op, "Operands must be numbers.");
+            if ((left is int && right is int) || (left is double && right is double)) return;
+            throw new StdError.RuntimeError(op, "TypeError: Cannot operate on numbers of different types.");
         }
 
-        private void CheckZeroDivisor(Parser.Token op, Object right)
+        private void CheckZeroDivisor(Token op, Object right)
         {
             if (right is Double && !((Double) right == 0)) { return; }
-            throw new RuntimeError(op, "Division by zero.");
+            throw new StdError.RuntimeError(op, "ZeroDivisionError: Divisor must be non-zero.");
         }
         private bool IsTruthy(object obj)
         {
@@ -105,48 +106,145 @@ namespace CODE_interpreter
         {
             return expr.Accept(this);
         }
-        
-        public Object VisitBinaryExpr(Expression.Binary expr)
+
+        private void Execute(Statement stmt)
+        {
+            stmt.Accept(this);
+        }
+
+        public Object VisitIfBlockStatement(Statement.IfBlock stmt)
+        {
+            ExecuteIfBlock(stmt.Statements);
+            return null;
+        }
+
+        public object VisitWhileBlockStatement(Statement.WhileBlock stmt)
+        {
+            ExecuteWhileBlock(stmt.Statements);
+            return null;
+        }
+
+        public Object VisitExpressionStatement(Statement.Expression stmt)
+        {
+            Evaluate(stmt.Expr);
+            return null;
+        }
+
+        private void ExecuteIfBlock(List<Statement> stmts)
+        {
+            foreach (Statement s in stmts)
+            {
+                Execute(s);
+            }
+        }
+        private void ExecuteWhileBlock(List<Statement> stmts)
+        {
+            foreach (Statement s in stmts)
+            {
+                Execute(s);
+            }
+        }
+
+        public Object VisitIfStatement(Statement.If stmt)
+        {
+            if (IsTruthy(Evaluate(stmt.Condition)))
+            {
+                ExecuteIfBlock(stmt.ThenBranch);
+            }
+            else if (stmt.ElseBranch != null)
+            {
+                ExecuteIfBlock(stmt.ElseBranch);
+            }
+            return null;
+        }
+
+        public Object VisitPrintStatement(Statement.Print stmt)
+        {
+            object value = Evaluate(stmt.Expr);
+            Console.WriteLine(Stringify(value));
+            return null;
+        }
+
+        public Object VisitVarStatement(Statement.Var stmt)
+        {
+            object value = null;
+            if (stmt.Initializer != null)
+            {
+                value = Evaluate(stmt.Initializer);
+            }
+
+            Env.DefineVar(stmt.Name, value);
+            return null;
+        }
+
+        public Object VisitWhileStatement(Statement.While stmt)
+        {
+            while (IsTruthy(Evaluate(stmt.Condition)))
+            {
+                ExecuteWhileBlock(stmt.Body);
+            }
+            return null;
+        }
+        public Object VisitAssignExpression(Expression.Assign expr)
+        {
+            Object value = Evaluate(expr.Value);
+            Env.Assign(expr.Name, value);
+            return value;
+        }
+
+        public Object VisitBinaryExpression(Expression.Binary expr)
         {
             Object left = Evaluate(expr.Left);
             Object right = Evaluate(expr.Right);
 
-            switch (expr.Operator.CurrType)
+            switch (expr.Operator.TokenType)
             {
-                case Parser.Token.Type.CONCAT:
-                    if (left is string && right is string)
-                    {
-                        return (string)left + (string)right;
-                    }
-                    throw new RuntimeError(expr.Operator, "Operands must be two strings.");
-                case Parser.Token.Type.ADD:
+                case Token.Type.CONCAT:
+                    return left.ToString() + right.ToString();
+                case Token.Type.ADD:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left + (int)right; }
+                    if (left is double && right is double) { return (double)left + (double)right; }
                     return (double)left + (double)right;
-                case Parser.Token.Type.SUB:
+                case Token.Type.SUB:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left - (int)right; }
+                    if (left is double && right is double) { return (double)left - (double)right; }
                     return (double)left - (double)right;
-                case Parser.Token.Type.DIV:
+                case Token.Type.DIV:
                     CheckNumberOperands(expr.Operator, left, right);
                     CheckZeroDivisor(expr.Operator, right);
+                    if (left is int && right is int) { return (int)left / (int)right; }
+                    if (left is double && right is double) { return (double)left / (double)right; }
                     return (double)left / (double)right;
-                case Parser.Token.Type.MULT:
+                case Token.Type.MULT:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left * (int)right; }
+                    if (left is double && right is double) { return (double)left * (double)right; }
                     return (double)left * (double)right;
-                case Parser.Token.Type.GREATER:
+                case Token.Type.GREATER:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left > (int)right; }
+                    if (left is double && right is double) { return (double)left > (double)right; }
                     return (double)left > (double)right;
-                case Parser.Token.Type.GREATER_EQUAL:
+                case Token.Type.GREATER_EQUAL:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left >= (int)right; }
+                    if (left is double && right is double) { return (double)left >= (double)right; }
                     return (double)left >= (double)right;
-                case Parser.Token.Type.LESSER:
+                case Token.Type.LESSER:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left < (int)right; }
+                    if (left is double && right is double) { return (double)left < (double)right; }
                     return (double)left < (double)right;
-                case Parser.Token.Type.LESSER_EQUAL:
+                case Token.Type.LESSER_EQUAL:
                     CheckNumberOperands(expr.Operator, left, right);
+                    if (left is int && right is int) { return (int)left <= (int)right; }
+                    if (left is double && right is double) { return (double)left <= (double)right; }
                     return (double)left <= (double)right;
-                case Parser.Token.Type.NOT_EQUAL: 
+                case Token.Type.NOT_EQUAL: 
                     return !IsEqual(left, right);
-                case Parser.Token.Type.EQUAL: 
+                case Token.Type.EQUAL: 
                     return IsEqual(left, right);
             }
 
