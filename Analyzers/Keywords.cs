c383b5eb437386013ffc150f0efc1768aa5b98b6
ShySpaceSheep <theminecraftf303@gmail.com>
2024-05-14 23:56:49 +0800

    Added dedicated class for reserved keywords

---
+++ Analyzers/Keywords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODEInterpreter.Analyzers
+{
+    public static class Keywords
+    {
+        private static readonly Dictionary<string, Token.Type> _rk = new()
+        {
+            {"CODE", Token.Type.CODE},
+            {"BEGIN", Token.Type.BLOCK_START},
+            {"END", Token.Type.BLOCK_END},
+            {"INT", Token.Type.VAR_INT},
+            {"CHAR", Token.Type.VAR_CHAR},
+            {"BOOL", Token.Type.VAR_BOOL},
+            {"FLOAT", Token.Type.VAR_FLOAT},
+            {"IF", Token.Type.IF},
+            {"ELSE", Token.Type.ELSE},
+            {"WHILE", Token.Type.WHILE},
+            {"DISPLAY", Token.Type.DISPLAY},
+            {"SCAN", Token.Type.SCAN},
+            {"AND", Token.Type.AND},
+            {"OR", Token.Type.OR},
+            {"NOT", Token.Type.NOT}
+        };
+
+        public static Dictionary<string, Token.Type> GetReservedKeywords() { return _rk; }
+    }
+}
