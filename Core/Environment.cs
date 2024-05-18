9405426597ef790db8500029a6638056a3bb4c66
ShySpaceSheep <theminecraftf303@gmail.com>
2024-05-18 22:04:16 +0800

    Updated `Environment` to be able to support data type tracking

--- Core/Environment.cs
+++ Core/Environment.cs
@@ -9,31 +9,54 @@ namespace CODEInterpreter
 {
     public class Environment
     {
-        private readonly Dictionary<string, object> _memory = new();
+        private readonly Dictionary<string, Token.Type> _typeMemory = new();
+        private readonly Dictionary<string, object> _dataMemory = new();
 
-        public void DefineVar(Token name, Object value)
+        public void DefineVar(Token.Type dataType, Token name, Object value)
         {
-            if (_memory.ContainsKey(name.Lexeme))
+            if (_dataMemory.ContainsKey(name.Lexeme))
             {
                 throw new StdError.RuntimeError(name, NameError.RedefinedVar(name.Lexeme));
             }
-            _memory.Add(name.Lexeme, value);
+            _typeMemory.Add(name.Lexeme, dataType);
+            _dataMemory.Add(name.Lexeme, value);
         }
 
         public void Assign(Token name, Object value)
         {
-            if (_memory.ContainsKey(name.Lexeme))
+            if (_typeMemory.TryGetValue(name.Lexeme, out Token.Type varDataType))
             {
-                _memory[name.Lexeme] = value;
+                if (!IsCompatibleDataType(varDataType, value)) { 
+                    throw new StdError.RuntimeError(name, TypeError.IncompatibleType(name.Lexeme, varDataType.ToString()));
+                }
+                _dataMemory[name.Lexeme] = value;
                 return;
             }
+
             throw new StdError.RuntimeError(name, NameError.UndefinedVar(name.Lexeme));
         }
 
         public object Get(Token name)
         {
-            if (_memory.TryGetValue(name.Lexeme, out object value)) { return value; }
+            if (_dataMemory.TryGetValue(name.Lexeme, out object value)) { return value; }
             throw new StdError.RuntimeError(name, NameError.UndefinedVar(name.Lexeme));
+        }
+
+        private bool IsCompatibleDataType(Token.Type dataType, Object value)
+        {
+            switch (dataType)
+            {
+                case Token.Type.VAR_INT:
+                    return value is int;
+                case Token.Type.VAR_FLOAT:
+                    return value is double;
+                case Token.Type.VAR_CHAR:
+                    return value is char;
+                case Token.Type.VAR_BOOL:
+                    return value is bool;
+                default:
+                    return false;
+            }
         }
     }
 }
